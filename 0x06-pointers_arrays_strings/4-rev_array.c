90618d0177c8a2d818f06101317ca05aa14550ed
livylo <livylo@users.noreply.github.com>
2022-06-16 08:38:59 -0700

    task 4 added to 0x06.C

---
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * reverse_array - reverses an array of integers
+ * @a: array of integers
+ * @n: number of elements in the array
+ */
+
+void reverse_array(int *a, int n)
+{
+int i, j;
+int temp;
+
+for (i = 0, j = n - 1; i < j; i++, j--)
+{
+temp = a[i];
+a[i] = a[j];
+a[j] = temp;
+}
+}
