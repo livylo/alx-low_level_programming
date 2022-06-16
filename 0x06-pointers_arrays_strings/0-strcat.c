59c685a5ced236f999b61371fff8f7bce0891fe0
livylo <livylo@users.noreply.github.com>
2022-06-16 07:47:37 -0700

    task 0 added to 0x06.c

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,33 @@
+#include "main.h"
+
+/**
+ * _strcat - concatenate two strings
+ * @dest: destination string
+ * @src: source string
+ * Return: pointer to resulting dest string
+ *
+
+char *_strcat(char *dest, char *src)
+{
+int i;
+int len1 = 0;
+int len2 = 0;
+
+for (i = 0; dest[i] != '\0'; i++)
+{
+len1++;
+}
+
+for (i = 0; src[i] != '\0'; i++)
+{
+len2++;
+}
+
+for (i = 0; i <= len2; i++)
+{
+dest[len1 + i] = src[i];
+}
+dest[len1 + i] = '\0';
+
+return (dest);
+}
