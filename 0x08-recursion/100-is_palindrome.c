c83d8d3aae3f84c2881609471de81456e11cbb75
livylo <livylo@users.noreply.github.com>
2022-06-22 05:21:15 -0700

    task 7 added to 0x08.C

--- 0x08-recursion/100-is_palindrome.c
+++ 0x08-recursion/100-is_palindrome.c
@@ -38,10 +38,11 @@ return (1 + _strlen_recursion(s + 1));
  * Return: 1 if palindrome, 0 if not
  */
 int check_pal(char *s, int i, int len)
+
 {
 if (*(s + i) != *(s + len - 1))
 return (0);
 if (i >= len)
 return (1);
-return (check_pal(s, i + 1, len - 1))
+return (check_pal(s, i + 1, len - 1));
 }
